3cedb27ca9b788b9f2118280cfad5b59e5b09a2e
Akmal Kamalov <neroxt@yandex.ru>
2023-07-02 18:20:13 +0400

    Update build

--- Build/Build.framework.js
+++ Build/Build.framework.js
@@ -1298,7 +1298,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3207184;
+STATICTOP = STATIC_BASE + 3207520;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3348,7 +3348,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3207184;
+var STATIC_BUMP = 3207520;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
