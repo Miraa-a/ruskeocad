f5149b959152fde9cc073a128e7c1374a92ecc2c
Akmal Kamalov <neroxt@yandex.ru>
2023-11-12 17:44:41 +0400

    Fixes

--- Build/Build.framework.js
+++ Build/Build.framework.js
@@ -1975,13 +1975,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  3584492: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 3584547: function($0) {performance.now = function() { return $0; };},  
- 3584595: function($0) {performance.now = function() { return $0; };},  
- 3584643: function() {performance.now = Module['emscripten_get_now_backup'];},  
- 3584698: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 3584759: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 3584823: function() {return Module.webglContextAttributes.powerPreference;}
+  3584508: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 3584563: function($0) {performance.now = function() { return $0; };},  
+ 3584611: function($0) {performance.now = function() { return $0; };},  
+ 3584659: function() {performance.now = Module['emscripten_get_now_backup'];},  
+ 3584714: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 3584775: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 3584839: function() {return Module.webglContextAttributes.powerPreference;}
 };
 
 
